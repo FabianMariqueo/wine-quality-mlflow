f0fffdff1f2f91039ca5b99943a94a74247aea38
Fabian <f.mariqueo01@ufromail.cl>
2019-10-11 13:38:06 -0300

    cambiando variables de entorno por archivos

--- train.py
+++ train.py
@@ -39,4 +39,7 @@ with mlflow.start_run():
     artifact_path = artifact_path.replace("file://", "")
     print(artifact_path)
 
-    os.environ["MLFLOW_ARTIFACT_PATH"] = artifact_path
+    f = open("mlflow_run.txt", "w")
+    f.write("mlflow models serve -m "+artifact_path+"/model -h 0.0.0.0 -p 1234")
+    f.close()
+    #os.environ["MLFLOW_ARTIFACT_PATH"] = artifact_path
